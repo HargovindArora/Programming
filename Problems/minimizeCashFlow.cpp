aef2169993f34c0bc8deca18f603ba89d858ca09
Hargovind Singh Arora <hargovinds50@gmail.com>
2020-06-14 23:00:22 +0530

    Minimize CashFlow

--- Problems/minimizeCashFlow.cpp
+++ Problems/minimizeCashFlow.cpp
@@ -17,6 +17,40 @@ void solve()
         arr[p1] -= amount;
         arr[p2] += amount;
     }
+    multiset<pair<int, int>> s;
+    for (int i = 0; i < p; i++)
+    {
+        s.insert(make_pair(arr[i], i));
+    }
+
+    while (!s.empty())
+    {
+
+        auto front = *(s.begin());
+        s.erase(s.begin());
+        auto end = *(prev(s.end()));
+        s.erase(prev(s.end()));
+
+        int front_amount = front.first;
+        int front_person = front.second;
+        int end_amount = end.first;
+        int end_person = end.second;
+
+        int settle_amount = min(-1 * front_amount, end_amount);
+        front_amount += settle_amount;
+        end_amount -= settle_amount;
+
+        cout << "Person " << front_person << " pays " << settle_amount << " amount to person " << end_person << endl;
+
+        if (front_amount)
+        {
+            s.insert(make_pair(front_amount, front_person));
+        }
+        if (end_amount)
+        {
+            s.insert(make_pair(end_amount, end_person));
+        }
+    }
 }
 
 int main(void)
@@ -25,13 +59,14 @@ int main(void)
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int t;
-    cin >> t;
-    while (t--)
-    {
+    // int t;
+    // cin >> t;
+    // while (t--)
+    // {
 
-        solve();
-    }
+    //     solve();
+    // }
+    solve();
 
     return 0;
 }
