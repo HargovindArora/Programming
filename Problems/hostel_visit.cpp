7ce151de75d043826faf80088480ff55888e251f
Hargovind Singh Arora <hargovinds50@gmail.com>
2020-07-11 22:31:03 +0530

    Debugged Hostel Visit

--- Problems/hostel_visit.cpp
+++ Problems/hostel_visit.cpp
@@ -1,74 +1,35 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <queue>
 #define ll long long int
 using namespace std;
 
-int main(){
-
-    int q,k;
-    cin >> q >> k;
-    multiset<ll> s;
-
-    for(int i=1; i<=q; i++){
-
-        int type;
-        cin >> type;
-
-        if(type==1){
-
-            ll x,y;
-            cin >> x >> y;
-            x = x*x;
-            y = y*y;
-            ll dist = x+y;
-            s.insert(dist);
-
-        }
-        else{
-
-            auto it = s.begin();
-            for(int i=0; i<k-1; i++){
-                it++;
+ll rocketDistance(ll x, ll y) {
+    return x*x + y*y;
+}
+int main(int argc, char const *argv[])
+{
+    int q, k, x, y, type;
+    cin>>q>>k;
+    priority_queue<ll> pq;
+    for(ll i=0;i<q;i++) {
+        cin>>type;
+        if(type==1) {
+            cin>>x>>y;
+            if(pq.size()==k) {
+                if(rocketDistance(x, y)<pq.top()) {
+                    pq.pop();
+                    pq.push(rocketDistance(x, y));
+                }
+            } else {
+                pq.push(rocketDistance(x, y));
             }
-            cout << *it << endl;
-
+        } else {
+            cout<<pq.top()<<endl;
+            cout << pq.size() << endl;
         }
-
     }
-
     return 0;
 }
-
-// #include <iostream>
-// #include <queue>
-// #define ll long long int
-// using namespace std;
-
-// ll rocketDistance(ll x, ll y) {
-//     return x*x + y*y;
-// }
-// int main(int argc, char const *argv[])
-// {
-//     int q, k, x, y, type;
-//     cin>>q>>k;
-//     priority_queue<ll> pq;
-//     for(ll i=0;i<q;i++) {
-//         cin>>type;
-//         if(type==1) {
-//             cin>>x>>y;
-//             if(pq.size()==k) {
-//                 if(rocketDistance(x, y)<pq.top()) {
-//                     pq.pop();
-//                     pq.push(rocketDistance(x, y));
-//                 }
-//             } else {
-//                 pq.push(rocketDistance(x, y));
-//             }
-//         } else {
-//             cout<<pq.top()<<endl;
-//         }
-//     }
-//     return 0;
-// }
 
 // #include <iostream>
 // #include <set>
