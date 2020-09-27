5bd5ede8defd5f02f29741c26af08e8d069270c7
Hargovind Singh Arora <hargovinds50@gmail.com>
2020-09-27 20:54:14 +0530

    experiment

--- experiment.cpp
+++ experiment.cpp
@@ -1,21 +1,26 @@
 #include<bits/stdc++.h>
 #define ll long long int
-
+#define endl '\n' 
 using namespace std;
 
-int main(){
-	ll n, count = 0;
-  	cin >> n;
-  	char ch;
-  	for( int i = 0; i < n; i++ ){
-    	for( int j = 0; j < n; j++ ){
-    	    cin >> ch;
-          	if( ch == 'D' ){
-            	count++;
-            }
-        }
-    }
-  	ll ans = sqrt( count ); // Sqrt of large number will overflow, apna method implement kar sqrt nikaalne ka
-  	cout << ans << '\n';
-  	return 0;
+void solve(){
+	
+	
+	
+}
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int t;
+	cin >> t;
+	while(t--){
+		
+		solve();
+		
+	}
+
+	return 0;
 }
