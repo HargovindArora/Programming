4124a62f3c3647f9e96a1d839565b2ecd2d323cf
Hargovind Singh Arora <hargovinds50@gmail.com>
2020-09-27 20:53:24 +0530

    Small changes and updates

--- Hashing/minWindowSubstring.cpp
+++ Hashing/minWindowSubstring.cpp
@@ -7,23 +7,27 @@ string minWindowSubstring(string str, string pat){
 
     int len1 = str.size();
     int len2 = pat.size();
+
     if(len1<len2){
         return "No Window";
     }
+
     int hash_str[256] = {0};
     int hash_pat[256] = {0};
     for(int i=0; i<len2; i++){
         hash_pat[pat[i]]++;
     }
+
     int start = 0, start_index = -1, min_len = INT_MAX;
     int count = 0;
+
     for(int j=0; j<len1; j++){
         hash_str[str[j]]++;
         if(hash_pat[str[j]]!=0 and hash_str[str[j]]<=hash_pat[str[j]]){
             count++;
         }
         if(count==len2){
-            while(hash_str[str[start]]>hash_pat[str[start]] or hash_pat[str[start]]==0){
+            while(hash_pat[str[start]]==0 or hash_str[str[start]]>hash_pat[str[start]]){
                 if(hash_str[str[start]]>hash_pat[str[start]]){
                     hash_str[str[start]]--;
                 }
@@ -36,11 +40,11 @@ string minWindowSubstring(string str, string pat){
             }
         }
     }
+
     if(start_index == -1){
         return "No Window";
     }
     return str.substr(start_index, min_len);
-
 }
 
 int main(){
--- experiment.cpp
+++ experiment.cpp
@@ -1,10 +1,21 @@
 #include<bits/stdc++.h>
+#define ll long long int
+
 using namespace std;
 
 int main(){
-
-    string k(7, 'k');
-    cout << k << endl;
-
-    return 0;
+	ll n, count = 0;
+  	cin >> n;
+  	char ch;
+  	for( int i = 0; i < n; i++ ){
+    	for( int j = 0; j < n; j++ ){
+    	    cin >> ch;
+          	if( ch == 'D' ){
+            	count++;
+            }
+        }
+    }
+  	ll ans = sqrt( count ); // Sqrt of large number will overflow, apna method implement kar sqrt nikaalne ka
+  	cout << ans << '\n';
+  	return 0;
 }
