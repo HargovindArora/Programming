8979ba6fb3fc67b2892e9ff343f1b382c98bc1b6
Hargovind Singh Arora <hargovinds50@gmail.com>
2020-10-01 22:05:40 +0530

    Update little_artem.cpp

--- CodeForces_contests/little_artem.cpp
+++ CodeForces_contests/little_artem.cpp
@@ -6,7 +6,7 @@ void solve(){
     int n,m;
     cin >> n >> m;
     string black_row(m, 'B');
-    vector<string> result(n, black_row);
+    vector<string> result(n, black_row); // Create a String Vector of all Black Rows first
     result[0][0] = 'W';
     for(int i=0; i<n; i++){
         cout << result[i] << endl;
